f6eeae4703229dc032bc48a76a7d683c17481396
wpxp123456 <2677556700@qq.com>
2025-06-05 19:15:28 +0800

    feat: supplement docs document (#4)

---
+++ content/en-US/guides/docs/features/_meta.tsx
@@ -0,0 +1,18 @@
+import ProTag from '@/components/pro-tag'
+
+export default {
+  'core': 'Core',
+  'hyperlink': 'Hyperlink',
+  'floating-images': 'Floating Images',
+  'thread-comment': 'Thread Comment',
+  'watermark': 'Watermark',
+  'print': {
+    title: <ProTag>Print</ProTag>,
+  },
+  'import-export': {
+    title: <ProTag>Import & Export</ProTag>,
+  },
+  'collaboration': {
+    title: <ProTag>Collaborative Editing</ProTag>,
+  },
+}
---
+++ content/zh-CN/guides/docs/features/_meta.tsx
@@ -0,0 +1,18 @@
+import ProTag from '@/components/pro-tag'
+
+export default {
+  'core': '核心功能',
+  'hyperlink': '超链接',
+  'floating-images': '浮动图片',
+  'thread-comment': '评论 / 批注',
+  'watermark': '水印',
+  'print': {
+    title: <ProTag>打印</ProTag>,
+  },
+  'import-export': {
+    title: <ProTag>导入导出</ProTag>,
+  },
+  'collaboration': {
+    title: <ProTag>协同编辑</ProTag>,
+  },
+}
