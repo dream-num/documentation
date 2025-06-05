d8e94a762746abfb7e46ce238ee286994ddc0e5e
wpxp@123456 <2677556700@qq.com>
2025-06-05 18:41:52 +0800

    feat: supplement docs document

--- content/en-US/guides/docs/features/_meta.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  'core': 'Core',
-  'hyperlink': 'Hyperlink',
-  'floating-images': 'Floating Images',
-  'thread-comment': 'Thread Comment',
-  'watermark': 'Watermark',
-}
--- content/zh-CN/guides/docs/features/_meta.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  'core': '核心功能',
-  'hyperlink': '超链接',
-  'floating-images': '浮动图片',
-  'thread-comment': '评论 / 批注',
-  'watermark': '水印',
-}
