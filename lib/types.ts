70b84c848ec3512f6f349fa54a40c146686291df
白熱 <sonne@asaki.me>
2025-06-18 00:06:50 +0800

    feat: add AutoTypeTable component and update types

---
+++ lib/types.ts
@@ -0,0 +1,5 @@
+export { FUniver } from '@univerjs/core/facade'
+
+export interface MyInterface {
+  name: string
+}
--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,10 +1,17 @@
 import type { MDXComponents } from 'mdx/types'
+import { createGenerator } from 'fumadocs-typescript'
+import { AutoTypeTable } from 'fumadocs-typescript/ui'
 import defaultMdxComponents from 'fumadocs-ui/mdx'
+
+const generator = createGenerator()
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     ...components,
+    AutoTypeTable: props => (
+      <AutoTypeTable {...props} generator={generator} />
+    ),
   }
 }
