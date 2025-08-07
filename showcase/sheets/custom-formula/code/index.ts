9b42288c57b6761dbb69ed626d546040f7a7e7d0
Wpxp123456 <2677556700@qq.com>
2025-08-07 19:13:50 +0800

    fix: fix custom-formula showcase

--- showcase/sheets/custom-formula/code/index.ts
+++ showcase/sheets/custom-formula/code/index.ts
@@ -1,6 +1,7 @@
 import { UniverSheetsCorePreset } from '@univerjs/preset-sheets-core'
 import sheetsCoreEnUS from '@univerjs/preset-sheets-core/locales/en-US'
 import { createUniver, LocaleType, mergeLocales } from '@univerjs/presets'
+import { FUNCTION_LIST_USER, functionEnUS, functionUser } from './custom-function'
 import { WORKBOOK_DATA } from './data'
 
 import './styles.css'
@@ -12,11 +13,16 @@ const { univerAPI } = createUniver({
   locales: {
     [LocaleType.EN_US]: mergeLocales(
       sheetsCoreEnUS,
+      functionEnUS,
     ),
   },
   presets: [
     UniverSheetsCorePreset({
       container: 'app',
+      formula: {
+        function: functionUser,
+        description: FUNCTION_LIST_USER,
+      },
     }),
   ],
 })
