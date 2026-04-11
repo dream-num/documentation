eb35c905aac0dd3f61acafb013a53046ad6aff5a
wpxp123456 <2677556700@qq.com>
2026-04-11 18:43:40 +0800

    fix: fix tag version link (#100)

--- app/[lang]/(home)/page.tsx
+++ app/[lang]/(home)/page.tsx
@@ -85,7 +85,7 @@ export default async function Page({ params }: IProps) {
                   dark:bg-neutral-800
                   dark:hover:bg-neutral-900
                 `}
-                href="https://github.com/dream-num/univer/releases/tag/v0.18.0"
+                href={`https://github.com/dream-num/univer/releases/tag/v${pkg.version}`}
               >
                 <span
                   className={`
